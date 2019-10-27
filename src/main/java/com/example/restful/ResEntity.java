dcf0319855b4205e38dd24942c9a5cc759723cf4
AngryBird <AngryBird@192.168.0.11>
2019-10-27 22:29:11 +0800

    update

---
+++ src/main/java/com/example/restful/ResEntity.java
@@ -0,0 +1,22 @@
+package com.example.restful;
+
+public class ResEntity {
+	private String hello;
+	private String value;
+
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
