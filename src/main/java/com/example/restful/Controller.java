26d053a256d947330c4a757a3c11db94a24ae8d5
AngryBird <AngryBird@192.168.0.11>
2019-10-27 22:28:43 +0800

    update

--- src/main/java/com/example/restful/Controller.java
+++ src/main/java/com/example/restful/Controller.java
@@ -4,16 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 @RestController
 public class Controller {
-	
-	@RequestMapping(value="/hello", method=RequestMethod.GET)
-	public String hello(){
-		return "hello~";
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
+	public ResEntity hello() {
+		ResEntity e = new ResEntity();
+		e.setHello("wilson");
+		return e;
 	}
-	
-	@RequestMapping(value="/hi", method=RequestMethod.POST)
-	public String hi(){
+
+	@RequestMapping(value = "/hi", method = RequestMethod.POST)
+	public String hi() {
 		return "hi~";
 	}
 }
